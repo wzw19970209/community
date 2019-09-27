9fdcfe48f48fa5a8b95d35756abc41267a87fc91
wzw19970209 <1479061365@qq.com>
2019-09-27 13:06:10 +0800

    ceshi

--- src/main/java/spring_boot/demo/controller/Greeting.java
+++ src/main/java/spring_boot/demo/controller/Greeting.java
@@ -1,6 +1,7 @@
 package spring_boot.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
